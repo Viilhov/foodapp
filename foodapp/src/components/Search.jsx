141adc8b334a625da4af793612d5ac57c7eee7c0
Viilhov <133661770+Viilhov@users.noreply.github.com>
2023-11-02 15:06:45 +0100

    Added await data functions

--- foodapp/src/components/Search.jsx
+++ foodapp/src/components/Search.jsx
@@ -1,10 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "25a4db9ac8dd426f8839c0b8bd5fcafb";
 
 export default function Search() {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState("pizza");
+  //Syntax of the useEffect hoo
+  useEffect(() => {
+    async function fetchFood() {
+      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data.results
+        );
+    }
+    fetchFood();
+  }, [query]);
   return (
     <div>
-      <input type="text" />
+      <input
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        type="text"
+      />
     </div>
   );
 }
