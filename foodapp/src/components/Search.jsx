30801ada40ab235a3343eb04e21f78317158f7d0
Viilhov <133661770+Viilhov@users.noreply.github.com>
2023-11-02 17:12:06 +0100

    Added title, button and picture to the app

--- foodapp/src/components/Search.jsx
+++ foodapp/src/components/Search.jsx
@@ -2,15 +2,15 @@ import { useEffect, useState } from "react";
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = "25a4db9ac8dd426f8839c0b8bd5fcafb";
 
-export default function Search() {
+export default function Search({ foodData, setFoodData }) {
   const [query, setQuery] = useState("pizza");
-  //Syntax of the useEffect hoo
+  //Syntax of the useEffect hook
   useEffect(() => {
     async function fetchFood() {
       const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
       const data = await res.json();
-      console.log(data.results
-        );
+      console.log(data.results);
+      setFoodData(data.results);
     }
     fetchFood();
   }, [query]);
